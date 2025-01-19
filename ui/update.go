0e0fc55fead6fa3463b24163fe71d1e455d626aa
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2025-01-18 22:23:27 -0500

    ui/view ui/update

---
+++ ui/update.go
@@ -0,0 +1,110 @@
+package ui
+
+import (
+	"context"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	case devicePlay:
+		return m, listenForDeviceStop(m.device.StopCh())
+
+	case deviceStop:
+		m.playing = nil
+		return m, listenForDevicePlay(m.device.PlayCh())
+
+	case deviceClock:
+		if m.device.Playing() {
+			return m, listenForDeviceClock(m.device.ClockCh())
+		}
+
+	case deviceError:
+		m.errs = append(m.errs, msg)
+		return m, listenForDeviceErrors(m.device.ErrorsCh())
+
+	case tea.WindowSizeMsg:
+		m.viewport.dim(msg.Width, msg.Height)
+
+	case tea.KeyMsg:
+
+		switch msg.String() {
+
+		case "ctrl+c", "q":
+			if m.device.Playing() {
+				m.stop()
+			}
+			return m, tea.Quit
+
+		case "R":
+			if m.device.Playing() {
+				m.stop()
+			}
+			err := m.loadSequence(m.sequence.Path)
+			if err != nil {
+				m.errs = append(m.errs, err)
+			}
+
+		case "h", "left":
+			if m.selected.x > 0 {
+				m.selected.x--
+				m.groupX[m.groupNames[m.selected.y]] = m.selected.x
+			}
+
+		case "l", "right":
+			if m.selected.x < len(m.groups[m.groupNames[m.selected.y]])-1 {
+				m.selected.x++
+				m.groupX[m.groupNames[m.selected.y]] = m.selected.x
+			}
+
+		case "k", "up":
+			if m.selected.y > 0 {
+				m.selected.y--
+				m.selected.x = m.groupX[m.groupNames[m.selected.y]]
+			}
+
+		case "j", "down":
+			if m.selected.y < len(m.groupNames)-1 {
+				m.selected.y++
+				m.selected.x = m.groupX[m.groupNames[m.selected.y]]
+			}
+
+		case "0":
+			m.selected.x = 0
+			m.groupX[m.groupNames[m.selected.y]] = m.selected.x
+
+		case "$":
+			m.selected.x = len(m.groups[m.groupNames[m.selected.y]]) - 1
+			m.groupX[m.groupNames[m.selected.y]] = m.selected.x
+
+		case "g":
+			m.selected.y = 0
+			m.selected.x = m.groupX[m.groupNames[m.selected.y]]
+
+		case "G":
+			m.selected.y = len(m.groupNames) - 1
+			m.selected.x = m.groupX[m.groupNames[m.selected.y]]
+
+		case " ":
+			switch {
+			case m.device.Stopped():
+				for _, p := range m.sequence.Playable {
+					p.ClearStep()
+				}
+				p := m.groups[m.groupNames[m.selected.y]][m.selected.x]
+				m.playing = &m.selected
+				ctx, stop := context.WithCancel(context.Background())
+				m.stop = stop
+				m.device.Play(ctx, p, m.sequence.BPM, m.sequence.Loop, m.sequence.Sync)
+				return m, listenForDeviceClock(m.device.ClockCh())
+
+			case m.device.Playing():
+				m.stop()
+			}
+		}
+	}
+
+	return m, nil
+}
---
+++ ui/view.go
@@ -0,0 +1,65 @@
+package ui
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+func (m model) View() string {
+
+	var header string
+	var groups []string
+
+	st := style(lipgloss.NewStyle())
+
+	header = st.sequence().Render(fmt.Sprintf("%s; bpm: %f; loop: %v; sync: %s", m.sequence.Path, m.sequence.BPM, m.sequence.Loop, m.sequence.Sync))
+	header += st.state().Render(fmt.Sprintf("state: %s", m.device.State()))
+
+	if len(m.errs) > 0 {
+		var errstr []string
+		for _, err := range m.errs {
+			errstr = append(errstr, err.Error())
+		}
+		errstr = append(errstr, fmt.Sprintf("%d errors:", len(m.errs)))
+		slices.Reverse(errstr)
+		header += st.errors().Render(strings.Join(errstr, "\n"))
+	}
+	w := m.sequence.Warnings()
+	if len(w) > 0 {
+		header += st.warnings().Render(strings.Join(w, "\n"))
+	}
+
+	header = st.header(m.viewport.width).Render(header)
+
+	for gIdx, g := range m.groupNames {
+		var sb strings.Builder
+		for _, char := range g {
+			sb.WriteRune(char)
+			sb.WriteString("\n")
+		}
+		var playables []string
+		for pIdx, p := range m.groups[g] {
+			steps := p.Steps()
+			lines := strings.Split(steps, "\n")
+			chunkSize := 16
+			if len(lines) > chunkSize {
+				var chunks []string
+				for chunkSize < len(lines) {
+					lines, chunks = lines[chunkSize:], append(chunks, strings.Join(lines[0:chunkSize:chunkSize], "\n"))
+					chunks = append(chunks, "  ")
+				}
+				steps = lipgloss.JoinHorizontal(lipgloss.Top, append(chunks, strings.Join(lines, "\n"))...)
+			}
+			playables = append(playables, st.playable(
+				pIdx == m.selected.x && gIdx == m.selected.y,
+				m.playing != nil && pIdx == m.playing.x && gIdx == m.playing.y,
+			).Render(p.Title()+steps))
+		}
+		groups = append(groups, lipgloss.JoinHorizontal(lipgloss.Top, append([]string{st.groupName().Render(sb.String())}, playables...)...))
+	}
+
+	return m.viewport.view(header, groups, m.selected)
+}
